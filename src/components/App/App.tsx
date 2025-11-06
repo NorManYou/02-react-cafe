818c0ca43091a1ebc0009065ac9d34b90175f954
Vitalii Shumakov <fafde8120@gmail.com>
2025-11-06 21:23:08 +0100

    final

---
+++ src/components/App/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import type { Votes, VoteType } from '../../types/votes';
+import css from './App.module.css';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
+
+
+const initialVotes: Votes = { good: 0, neutral: 0, bad: 0 };
+
+const App = () => {
+    const [votes, setVotes] = useState<Votes>({ ...initialVotes });
+    const { good, neutral, bad } = votes;
+
+    const totalVotes = good + neutral + bad;
+    const positiveRate = totalVotes ? Math.round((good / totalVotes) * 100) : 0;
+    const isVotes = totalVotes > 0;
+
+    const handleVote = (type: VoteType) => {
+        setVotes(prev => ({ ...prev, [type]: prev[type] + 1 }));
+    }
+
+    const resetVotes = () => {
+        setVotes(initialVotes)
+    }
+
+    return (
+        <div className={css.app}>
+            <CafeInfo />
+            <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={isVotes} />
+            {isVotes ? <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} /> : <Notification />}
+        </div>);
+}
+
+
+export default App;
---
+++ src/components/VoteStats/VoteStats.tsx
@@ -0,0 +1,23 @@
+import type { Votes } from '../../types/votes';
+import css from './VoteStats.module.css'
+
+interface VoteStatsProps {
+    votes: Votes;
+    totalVotes: number;
+    positiveRate: number;
+}
+
+const VoteStats = ({ votes, totalVotes, positiveRate }: VoteStatsProps) => {
+    const { good, neutral, bad } = votes;
+    return (
+        <div className={css.container}>
+            <p className={css.stat}>Good: <strong>{good}</strong></p>
+            <p className={css.stat}>Neutral: <strong>{neutral}</strong></p>
+            <p className={css.stat}>Bad: <strong>{bad}</strong></p>
+            <p className={css.stat}>Total: <strong>{totalVotes}</strong></p>
+            <p className={css.stat}>Positive: <strong>{positiveRate}%</strong></p>
+        </div>
+    )
+}
+
+export default VoteStats;
